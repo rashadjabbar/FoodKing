f0a30353885f527e981b259786e9128ba021913c
Celal <calal.ismiyev@uniser.az>
2023-11-01 16:36:39 +0400

    Basket

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,21 +1,24 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AllCategory } from 'src/models/category';
 import { CategoryService } from 'src/services/category.service';
 import { appJs } from '../../assets/js/app.js'
 import { Router } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductService } from 'src/services/product.service';
 import { GlobalService } from 'src/services/global.service';
 import { BasketService } from 'src/services/public/basket.service';
+import { MatMenuTrigger } from '@angular/material/menu';
+
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrls: ['./public.component.scss'],
-
 })
+
 export class PublicComponent {
   message: any;
   totalBasketAmount?: number = 0;
+
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
 
   constructor(
     private router: Router,
@@ -55,6 +58,7 @@ export class PublicComponent {
   getBasket(){
     this.basketService.getBasket().subscribe(res => {
       this.basketItems = res.data.basketItems
+      console.log(this.basketItems)
      this.totalBasketAmount = isNaN(res.data.amountInfo?.amount + res.data.amountInfo?.serviceFee)? 0 : res.data.amountInfo?.amount + res.data.amountInfo?.serviceFee; 
     }) 
   }
@@ -65,4 +69,11 @@ export class PublicComponent {
      }) 
   }
 
+  closeBasket(){
+    this.trigger.closeMenu();
+  }
+
+  confirmOrder(){
+    
+  }
 }
