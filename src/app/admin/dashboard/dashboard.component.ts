9f0042dcc2deee7ae20e25be4235cabe87d25e96
Shahin9797 <shahin.mustafayev@uniser.az>
2024-02-15 17:14:58 +0400

    shahin

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -18,10 +18,11 @@ import {
   ApexTooltip,
   ApexStroke,
   ApexYAxis,
-  ApexMarkers
+  ApexMarkers,
 } from "ng-apexcharts";
 import { ElementRef } from '@angular/core';
 import { ApexOptions } from 'ng-apexcharts';
+import { ChartType } from 'ngx-apexcharts';
 
 export type ChartOptions = {
   series: ApexNonAxisChartSeries;
@@ -30,7 +31,16 @@ export type ChartOptions = {
   labels: any;
 };
 
-
+export type ChartOptions2 = {
+  series2: ApexAxisChartSeries;
+  chart2: ApexChart;
+  dataLabels2: ApexDataLabels;
+  stroke2: ApexStroke;
+  xaxis2: ApexXAxis;
+  yaxis2: ApexYAxis;
+  title2: ApexTitleSubtitle;
+  markers2: ApexMarkers;
+};
 
 @Component({
   selector: 'app-dashboard',
@@ -39,35 +49,121 @@ export type ChartOptions = {
 })
 export class DashboardComponent {
 
-  @ViewChild("chart") chart!: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
+   @ViewChild("chart") chart!: ChartComponent;
+   public chartOptions!: Partial<ChartOptions>
+   public chartOptions2!: Partial<ChartOptions2>
 
-  constructor() {
-    this.chartOptions = {
-      series: [44, 55, 13, 43, 22],
-      chart: {
-        width: 380,
-        type: "donut"
-      },
-      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-      responsive: [
+   constructor() {
+     this.chartOptions = {
+       series: [44, 55, 13, 43, 22],
+       chart: {
+         width: 380,
+         type: "donut"
+       },
+       labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+       responsive: [
+         {
+           breakpoint: 480,
+           options: {
+             chart: {
+               width: 200
+             },
+             legend: {
+               position: "bottom"
+             }
+           }
+         }
+       ]
+     };
+
+
+
+     this.chartOptions2 = {
+      series2: [
         {
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
+          name: "New York Temperature",
+          data: [
+            {
+              x: "Jan",
+              y: [-2, 4]
             },
-            legend: {
-              position: "bottom"
+            {
+              x: "Feb",
+              y: [-1, 6]
+            },
+            {
+              x: "Mar",
+              y: [3, 10]
+            },
+            {
+              x: "Apr",
+              y: [8, 16]
+            },
+            {
+              x: "May",
+              y: [13, 22]
+            },
+            {
+              x: "Jun",
+              y: [18, 26]
+            },
+            {
+              x: "Jul",
+              y: [21, 29]
+            },
+            {
+              x: "Aug",
+              y: [21, 28]
+            },
+            {
+              x: "Sep",
+              y: [17, 24]
+            },
+            {
+              x: "Oct",
+              y: [11, 18]
+            },
+            {
+              x: "Nov",
+              y: [6, 12]
+            },
+            {
+              x: "Dec",
+              y: [1, 7]
             }
+          ]
+        }
+      ],
+      chart2: {
+        height: 350,
+        type: "rangeArea"
+      },
+      stroke2: {
+        curve: "straight"
+      },
+      title2: {
+        text: "New York Temperature (all year round)"
+      },
+      markers2: {
+        hover: {
+          sizeOffset: 5
+        }
+      },
+      dataLabels2: {
+        enabled: false
+      },
+      yaxis2: {
+        labels: {
+          formatter: (val) => {
+            return val + "°C";
           }
         }
-      ]
-    };
-
-
+      }
+    }; 
 
     
-  }
+   }
 
+
+ 
 }
